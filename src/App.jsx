2affb65605a9da2c1d7985a6407d5d4a3600a652
Fu71 <mthienphu563@gmail.com>
2026-09-21 11:05:38 +0700

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import Home from "./pages/Home/Home.jsx";
 import Product from "./pages/Product/Product.jsx";
 import NotFound from "./pages/NotFound/NotFound.jsx";
-import React, { Component } from "react";
 import { Route, Routes } from "react-router";
-import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavbar from "./components/MyNavbar.jsx";
 
 export default function App() {
@@ -12,10 +11,11 @@ export default function App() {
       {/* <MyNavbar /> */}
           
       <Routes>
-        <Route element={<MyNavbar/>}
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="*" element={<NotFound/>} />
+        <Route element={<MyNavbar />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </>
   );
--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -5,19 +5,21 @@ import { Link, Outlet } from "react-router";
 
 function MyNavbar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/src/pages/Home/Home.jsx" as={Link}>Home</Nav.Link>
-            <Nav.Link href="/src/pages/Product/Product.jsx" as={Link}>Product</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <Outlet />
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/product">Product</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </>
   );
 }
 
